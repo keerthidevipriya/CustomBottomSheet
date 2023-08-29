c9ff106cdd9c6be34d2fa0ff6ad698551fdbe6e5
keerthidevipriya <keerthidevipriya@gmail.com>
2023-08-29 09:35:38 +0530

    My Bottom Sheet added

---
+++ CustomBottomSheet/MyBottomSheet.swift
@@ -0,0 +1,17 @@
+//
+//  MyBottomSheet.swift
+//  CustomBottomSheet
+//
+//  Created by Keerthi Devipriya(kdp) on 29/08/23.
+//
+
+import UIKit
+
+class MyBottomSheet: BottomSheetController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+}
--- CustomBottomSheet/ViewController.swift
+++ CustomBottomSheet/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: BottomSheetController {
+class ViewController: UIViewController {
     
     enum Constant {
         static let margin: CGFloat = 16
@@ -25,7 +25,8 @@ class ViewController: BottomSheetController {
         let btn = UIButton()
         btn.titleLabel?.textColor = .black
         btn.translatesAutoresizingMaskIntoConstraints = false
-        btn.addTarget(self, action: #selector(btnTapped), for: .touchUpInside)
+        btn.addTarget(self, action: #selector(gotoBtnTapped), for: .touchUpInside)
+        btn.tag = 1
         return btn
     }()
     
@@ -69,20 +70,20 @@ class ViewController: BottomSheetController {
     }
 
     func configure() {
-        self.containerView.backgroundColor = .clear
+        self.containerView.backgroundColor = .red
         fitBtn.setTitle("Fit", for: .normal)
-        smallBtn.setTitle("Small", for: .normal)
+        /*smallBtn.setTitle("Small", for: .normal)
         mediumBtn.setTitle("Medium", for: .normal)
         largeBtn.setTitle("Large", for: .normal)
-        fillBtn.setTitle("Fill", for: .normal)
+        fillBtn.setTitle("Fill", for: .normal)*/
     }
 
     func configureView() {
         containerView.addSubview(fitBtn)
-        containerView.addSubview(smallBtn)
+        /*containerView.addSubview(smallBtn)
         containerView.addSubview(mediumBtn)
         containerView.addSubview(largeBtn)
-        containerView.addSubview(fillBtn)
+        containerView.addSubview(fillBtn)*/
         self.view.addSubview(containerView)
     }
     
@@ -93,7 +94,7 @@ class ViewController: BottomSheetController {
 
             fitBtn.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
 
-            smallBtn.topAnchor.constraint(equalTo: fitBtn.bottomAnchor, constant: Constant.margin),
+            /*smallBtn.topAnchor.constraint(equalTo: fitBtn.bottomAnchor, constant: Constant.margin),
             mediumBtn.topAnchor.constraint(equalTo: smallBtn.bottomAnchor, constant: Constant.margin),
             largeBtn.topAnchor.constraint(equalTo: mediumBtn.bottomAnchor, constant: Constant.margin),
             fillBtn.topAnchor.constraint(equalTo: largeBtn.bottomAnchor, constant: Constant.margin),
@@ -101,14 +102,14 @@ class ViewController: BottomSheetController {
             smallBtn.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
             mediumBtn.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
             largeBtn.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
-            fillBtn.centerXAnchor.constraint(equalTo: containerView.centerXAnchor)
+            fillBtn.centerXAnchor.constraint(equalTo: containerView.centerXAnchor)*/
 
         ])
     }
 }
 
 extension ViewController {
-    @objc func btnTapped() {
+    @objc func gotoBtnTapped(sender: UIButton!) {
         print("btn taped--")
     }
     @objc func buttonAction() {
